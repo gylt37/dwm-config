d21756fce111149a39201e0f745af75a47dd7c14
gylt37 <gylt37@qq.com>
2021-09-13 12:38:45 +0800

    排版优化

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -1,51 +1,51 @@
 /* See LICENSE file for copyright and license details. */
 #include <X11/XF86keysym.h>
 /* appearance */
-static const unsigned int snap      = 50;       /* snap pixel */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int snap     = 50;  /* snap pixel */
+static const unsigned int borderpx = 3;   /* border pixel of windows */
 
-static const unsigned int gappih    = 4;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 4;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 4;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 4;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih = 4;  /* horiz inner gap between windows */
+static const unsigned int gappiv = 4;  /* vert inner gap between windows */
+static const unsigned int gappoh = 4;  /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov = 4;  /* vert outer gap between windows and screen edge */
 
-static const int vertpad            = 4;       /* vertical padding of bar */
-static const int sidepad            = 4;       /* horizontal padding of bar */
+static const int vertpad = 4;  /* vertical padding of bar */
+static const int sidepad = 4;  /* horizontal padding of bar */
 
-static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const Bool viewontag         = True;     /* Switch view on tag switch */
+static const int smartgaps  = 0;     /* 1 means no outer gap when there is only one window */
+static const int showbar    = 1;     /* 0 means no bar */
+static const int topbar     = 1;     /* 0 means bottom bar */
+static const Bool viewontag = True;  /* Switch view on tag switch */
 
 static const char *fonts[]          = {
     "Roboto Mono:size=12",
     "Noto Sans CJK SC:size=10",
     "Symbols Nerd Font:size=11"
 };
-static const char dmenufont[]       = "Noto Sans CJK SC:size=10";
+static const char dmenufont[] = "Noto Sans CJK SC:size=10";
 
-static const char col_gray1[]       = "#000000";
-static const char col_gray2[]       = "#cccccc";
-static const char col_gray3[]       = "#dddddd";
+static const char col_gray1[] = "#000000";
+static const char col_gray2[] = "#cccccc";
+static const char col_gray3[] = "#dddddd";
 
-static const char col_cyan1[]       = "#008080";
-static const char col_cyan2[]       = "#00ccff";
-static const char col_violet[]      = "#acacff";
+static const char col_cyan1[]  = "#008080";
+static const char col_cyan2[]  = "#00ccff";
+static const char col_violet[] = "#acacff";
 
 static const char *colors[][3]      = {
     /*                        fg         bg         border  */
-	[SchemeNorm]       = { col_gray2,  col_gray1,   col_gray2 },
-	[SchemeSel]        = { col_gray3,  col_cyan1,   col_cyan2 },
-	[SchemeHid]        = { col_violet, col_gray1,   col_cyan1 },
+	[SchemeNorm] = { col_gray2,  col_gray1,   col_gray2 },
+	[SchemeSel]  = { col_gray3,  col_cyan1,   col_cyan2 },
+	[SchemeHid]  = { col_violet, col_gray1,   col_cyan1 },
 };
 
-static const double defaultopacity  = 0.98;
+static const double defaultopacity = 0.98;
 
 static const unsigned int alphas[][3] = {
-	/*               fg     bg     border   */
+	/*             fg     bg     border */
 	[SchemeNorm] = { 0,  128,      0 },
 	[SchemeSel]  = { 0,  128,    255 },
-    [SchemeHid]  = { 0,   48,      0 },
+	[SchemeHid]  = { 0,   48,      0 },
 };
 
 typedef struct {
@@ -83,7 +83,7 @@ static const char *const autostart[] = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "", "6", "7", "8", "9" };
+static const char *tags[]           = { "", "", "", "", "", "6", "7", "8", "9" };
 static const char *defaulttagapps[] = {
     "alacritty",
     "/usr/bin/google-chrome-stable",
@@ -111,9 +111,9 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const float mfact     = 0.55;  /* factor of master area size [0.05..0.95] */
+static const int nmaster     = 1;     /* number of clients in master area */
+static const int resizehints = 1;     /* 1 means respect size hints in tiled resizals */
 
 #define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
 #include "vanitygaps.c"
@@ -137,16 +137,16 @@ static const Layout layouts[] = {
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static char dmenumon[2]       = "0";                    /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", NULL };
 
 /* commands */
-static const char *termcmd[]  = { "alacritty", NULL };
-static const char *rofidrun[] = { "rofi", "-show", "drun",NULL };
+static const char *termcmd[]    = { "alacritty", NULL };
+static const char *rofidrun[]   = { "rofi", "-show", "drun",NULL };
 static const char *rofiwindow[] = { "rofi", "-show", "window",NULL };
 
-static const char *polybar[]  = {"/home/gylt/.config/dwm/trayer.sh"};
-static const char *lock[]     = {"/home/gylt/.config/dwm/lock.sh"};
+static const char *polybar[] = {"/home/gylt/.config/dwm/trayer.sh"};
+static const char *lock[]    = {"/home/gylt/.config/dwm/lock.sh"};
 
 
 static Key keys[] = {
